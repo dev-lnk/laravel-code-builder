128364c26c8b3042342c7cee8cd4ee99f83b839c
levchenko-ivan <ivan97111@yandex.ru>
2024-08-27 12:35:18 +0300

    feat: TypeScriptBuilder

---
+++ src/Services/Builders/Advanced/Contracts/TypeScriptContract.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DevLnk\LaravelCodeBuilder\Services\Builders\Advanced\Contracts;
+
+use DevLnk\LaravelCodeBuilder\Services\Builders\BuilderContract;
+
+interface TypeScriptContract extends BuilderContract
+{
+}
